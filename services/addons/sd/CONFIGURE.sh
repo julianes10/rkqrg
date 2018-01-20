63ccd0dcba521f1d902a023c5dd1b3c6ec211c27
julianes10 <julian.espinosa.santos@ericsson.com>
2018-01-20 16:58:52 +0100

    sd working

--- services/addons/sd/CONFIGURE.sh
+++ services/addons/sd/CONFIGURE.sh
@@ -1,17 +1,21 @@
 #Pre-requisite
 PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
 chmod 755 ./*.sh
-cp sdhotupdate.sh /usr/local/bin/
-cp -rf sdhotupdate.service /lib/systemd/system/
+PLUGIN="plugin.video.SportsDevil"
+GIT_DIR="/home/pi/git/" 
+NAME="sdhotupdate"
 
-mkdir -p /home/pi/git
-cd /home/pi/git 
+
+cp "$NAME.sh" /usr/local/bin/
+cp -rf "$NAME.service" /lib/systemd/system/
+
+mkdir -p $GIT_DIR
+cd $GIT_DIR
 sudo apt-get install git
-git clone https://github.com/boudabass/SportsDevil.git
+git clone https://github.com/boudabass/SportsDevil.git $PLUGIN 
 
+chmod 644 /lib/systemd/system/"$NAME.service" 
+systemctl enable "$NAME.service"
+systemctl start  "$NAME.service"
+systemctl status "$NAME.service"
 
-
-chmod 644 /lib/systemd/system/sdhotupdate.service 
-systemctl enable sdhotupdate.service
-systemctl start sdhotupdate.service 
-systemctl status sdhotupdate.service 
--- services/addons/sd/sdhotupdate.sh
+++ services/addons/sd/sdhotupdate.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
 
-logger "$0 Updating SC..."
-cd /home/pi/git/SportsDevil 
+PLUGIN="plugin.video.SportsDevil"
+GIT_DIR="/home/pi/git/" 
+TARGET_DIR="/opt/retropie/configs/ports/kodi/addons"
+TO_ZIP_DIR=$PLUGIN 
+
+logger "$0 Updating addon SC..."
+cd $GIT_DIR/$PLUGIN
 git pull
-mkdir -p /opt/retropie/configs/ports/kodi/addons/plugin.video.SportsDevil/
-cp -rf * /opt/retropie/configs/ports/kodi/addons/plugin.video.SportsDevil/
-chown -R pi:pi /opt/retropie/configs/ports/kodi/addons/plugin.video.SportsDevil/
-logger "$0 Updating SC done"
+if [ -d "$TARGET_DIR/$PLUGIN" ]; then
+  logger "$0 Hot updating addon..."
+  cp -rf * "$TARGET_DIR/$PLUGIN/"
+  chown -R pi:pi "$TARGET_DIR/$PLUGIN/"
+fi
+logger "$0 Generate zip file "$PLUGIN.zip" ..."
+cd ..
+zip -r "$PLUGIN.zip" $PLUGIN   
+chown pi:pi "$PLUGIN.zip"
+logger "$0 addon SC updated"
 
