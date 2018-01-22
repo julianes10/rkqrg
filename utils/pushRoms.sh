ff7f6e47963b845994e50ed706e906f6670f45fc
julianes10 <julianes10@yahoo.com>
2018-01-22 16:19:52 +0000

    push and pull to sync

--- utils/pushRoms.sh
+++ utils/pushRoms.sh
@@ -3,8 +3,8 @@ echo "Using '$1' first parameter as source host to rsync"
 
 PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
 
-rsync -avhr ssh pi@$1:/home/pi/Retropie/roms/ /home/pi/Retropie/roms/
-# TODO --delete
+rsync -avhr ssh /home/pi/Retropie/roms/  pi@$1:/home/pi/Retropie/roms/ 
+# TODO -delete
 # TODO Should restart retropie
 
 
