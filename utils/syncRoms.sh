e936733caa1dd1897b63be7a91ccfc265955a17d
julianes10 <julian.espinosa.santos@ericsson.com>
2018-01-22 13:49:08 +0100

    fixes for compiling it

---
+++ utils/syncRoms.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+echo "Using '$1' first parameter as source host to rsync"
+
+PATH="$PATH:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin"
+
+rsync -avhr ssh pi@$1:/home/pi/Retropie/roms/ /home/pi/Retropie/roms/
+# TODO --delete
+# TODO Should restart retropie
+
+
